6ebc1b61ee83af18162df713e4bdea0ad831c126
Brauzi35 <57174753+Brauzi35@users.noreply.github.com>
2023-04-04 10:47:53 +0200

    Bug.java update

--- src/main/java/model/Bug.java
+++ src/main/java/model/Bug.java
@@ -2,4 +2,17 @@ package model;
 
 public class Bug {
     //to be defined, still have to decide on the metrics
+    private String key;
+    private Version fv;
+    private Version ov;
+    private Version iv;
+
+    public Bug(String key, Version fv, Version ov, Version iv) {
+        this.key = key;
+        this.fv = fv;
+        this.ov = ov;
+        this.iv = iv;
+    }
+
+
 }
